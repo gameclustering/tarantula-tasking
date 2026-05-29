74c7a907bffc91217d52a064dc25bac32e2d04b6
yingh <yinghu.lu@gmail.com>
2026-05-29 11:57:05 -0700

    feat: replace git_cmd.go with GitClient using go-git/v6
    Replace os/exec shell-out with go-git/v6 pure-Go library. GitClient
    is struct-based with SSH key auth via client.WithSSHAuth. Adds Clone
    and CheckoutTag not possible with the old approach. Removes orphaned
    util.GitPush() call from AppManager.Shutdown.

---
+++ internal/util/git_client.go
@@ -0,0 +1,159 @@
+package util
+
+import (
+	"fmt"
+	"time"
+
+	git "github.com/go-git/go-git/v6"
+	"github.com/go-git/go-git/v6/plumbing"
+	"github.com/go-git/go-git/v6/plumbing/client"
+	"github.com/go-git/go-git/v6/plumbing/object"
+	gossh "github.com/go-git/go-git/v6/plumbing/transport/ssh"
+)
+
+type GitClient struct {
+	Path        string
+	PrivateKey  string // PEM-encoded SSH private key for remote operations
+	AuthorName  string
+	AuthorEmail string
+	repo        *git.Repository
+}
+
+func (g *GitClient) Open() error {
+	r, err := git.PlainOpen(g.Path)
+	if err != nil {
+		return err
+	}
+	g.repo = r
+	return nil
+}
+
+func (g *GitClient) sshOpt() (client.Option, error) {
+	auth, err := gossh.NewPublicKeys("git", []byte(g.PrivateKey), "")
+	if err != nil {
+		return nil, err
+	}
+	return client.WithSSHAuth(auth), nil
+}
+
+func (g *GitClient) CurrentBranch() (string, error) {
+	head, err := g.repo.Head()
+	if err != nil {
+		return "", err
+	}
+	return head.Name().Short(), nil
+}
+
+func (g *GitClient) Add(path string) error {
+	w, err := g.repo.Worktree()
+	if err != nil {
+		return err
+	}
+	_, err = w.Add(path)
+	return err
+}
+
+func (g *GitClient) Remove(path string) error {
+	w, err := g.repo.Worktree()
+	if err != nil {
+		return err
+	}
+	_, err = w.Remove(path)
+	return err
+}
+
+func (g *GitClient) Commit(msg string) error {
+	w, err := g.repo.Worktree()
+	if err != nil {
+		return err
+	}
+	opts := &git.CommitOptions{}
+	if g.AuthorName != "" {
+		opts.Author = &object.Signature{
+			Name:  g.AuthorName,
+			Email: g.AuthorEmail,
+			When:  time.Now(),
+		}
+	}
+	_, err = w.Commit(msg, opts)
+	return err
+}
+
+func (g *GitClient) Push() error {
+	opt, err := g.sshOpt()
+	if err != nil {
+		return err
+	}
+	err = g.repo.Push(&git.PushOptions{ClientOptions: []client.Option{opt}})
+	if err == git.NoErrAlreadyUpToDate {
+		return nil
+	}
+	return err
+}
+
+func (g *GitClient) Pull() error {
+	w, err := g.repo.Worktree()
+	if err != nil {
+		return err
+	}
+	opt, err := g.sshOpt()
+	if err != nil {
+		return err
+	}
+	err = w.Pull(&git.PullOptions{ClientOptions: []client.Option{opt}})
+	if err == git.NoErrAlreadyUpToDate {
+		return nil
+	}
+	return err
+}
+
+func (g *GitClient) Status() (string, error) {
+	w, err := g.repo.Worktree()
+	if err != nil {
+		return "", err
+	}
+	st, err := w.Status()
+	if err != nil {
+		return "", err
+	}
+	return st.String(), nil
+}
+
+func (g *GitClient) Checkout(branch string) error {
+	w, err := g.repo.Worktree()
+	if err != nil {
+		return err
+	}
+	return w.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.NewBranchReferenceName(branch),
+	})
+}
+
+func (g *GitClient) Clone(url, path string) error {
+	opt, err := g.sshOpt()
+	if err != nil {
+		return err
+	}
+	r, err := git.PlainClone(path, &git.CloneOptions{
+		URL:           url,
+		ClientOptions: []client.Option{opt},
+	})
+	if err != nil {
+		return err
+	}
+	g.repo = r
+	g.Path = path
+	return nil
+}
+
+func (g *GitClient) CheckoutTag(tag string) error {
+	w, err := g.repo.Worktree()
+	if err != nil {
+		return err
+	}
+	ref, err := g.repo.Tag(tag)
+	if err != nil {
+		return fmt.Errorf("tag %s not found: %w", tag, err)
+	}
+	return w.Checkout(&git.CheckoutOptions{Hash: ref.Hash()})
+}
