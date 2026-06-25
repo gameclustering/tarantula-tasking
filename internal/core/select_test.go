b0075905390d86ec4ebd6f3087166e58e65cf0c4
yingh <yinghu.lu@gmail.com>
2026-06-24 22:43:38 -0700

    code-refactoring

--- internal/core/select_test.go
+++ internal/core/select_test.go
@@ -29,6 +29,36 @@ func TestSelectOne(t *testing.T) {
 	}
 }
 
+func TestSelectTimer(t *testing.T) {
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		for i := range 5 {
+			ch1 <- i + 1
+		}
+		close(ch1)
+	}()
+	go func() {
+		time.Sleep(1 * time.Second)
+		for i := range 5 {
+			ch2 <- i + 100
+		}
+		close(ch2)
+	}()
+	tc := time.NewTimer(500 * time.Millisecond)
+	defer tc.Stop()
+	select {
+	case x := <-ch1:
+		fmt.Printf("xfrom ch1 >> %d\n", x)
+	case y := <-ch2:
+		fmt.Printf("xfrom ch2 >> %d\n", y)
+	case <-tc.C:
+		fmt.Println("timeout")
+	}
+}
+
 func TestSelectBreak(t *testing.T) {
 	ch1 := make(chan int, 10)
 	ch2 := make(chan int, 10)
