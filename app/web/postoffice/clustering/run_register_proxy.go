061c6eda95b619c684674445735966c90555c659
yingh <yinghu.lu@gmail.com>
2026-06-26 00:52:02 -0700

    move sub sync to rpc calls from member list calls

--- app/web/postoffice/clustering/run_register_proxy.go
+++ app/web/postoffice/clustering/run_register_proxy.go
@@ -1,8 +1,19 @@
 package clustering
 
-import "gameclustering.com/internal/protocol"
+import (
+	"fmt"
+	"strings"
+
+	"gameclustering.com/internal/core"
+	"gameclustering.com/internal/protocol"
+)
 
 func (c *DataServiceProvider) runRegister(sub *protocol.Subscription) (*protocol.Response, error) {
-	
-	return &protocol.Response{Successful: true},nil
+	for _, m := range c.Members() {
+		parts := strings.Split(m.Address(), ":")
+		rpcEndpint := fmt.Sprintf("%s:%d", parts[0], core.RPC_PORT)
+		core.AppLog.Debug().Msgf("register on to %s", rpcEndpint)
+		
+	}
+	return &protocol.Response{Successful: true}, nil
 }
